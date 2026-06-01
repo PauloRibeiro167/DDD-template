ab8b3634989e9bb69ef02483c61d17bb3b8be2df
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-06-01 17:04:34 -0300

    fix: referenciar modulos globais no application contract da API

--- presets/api_lite/files/app/domains/shared/contracts/application_contract.rb
+++ presets/api_lite/files/app/domains/shared/contracts/application_contract.rb
@@ -2,7 +2,7 @@
 
 module Shared
   module Contracts
-    class ApplicationContract < Dry::Validation::Contract
+    class ApplicationContract < ::Dry::Validation::Contract
       UUID_FORMAT = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
 
       config.messages.default_locale = :"pt-BR"
@@ -19,7 +19,7 @@ module Shared
 
         normalized_value = value.strip.downcase
         values[key_name] = normalized_value
-        key.failure("deve ser um e-mail válido") unless URI::MailTo::EMAIL_REGEXP.match?(normalized_value)
+        key.failure("deve ser um e-mail válido") unless ::URI::MailTo::EMAIL_REGEXP.match?(normalized_value)
       end
 
       register_macro(:positive_integer) do
@@ -33,7 +33,7 @@ module Shared
       end
 
       register_macro(:iso8601_datetime) do
-        DateTime.iso8601(value.to_s)
+        ::DateTime.iso8601(value.to_s)
       rescue ArgumentError
         key.failure("deve ser uma data/hora ISO8601 válida")
       end
