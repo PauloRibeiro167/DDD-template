dab057856f9fc95f703bfab163e402103e31a1fb
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-06-01 17:04:20 -0300

    refactor: delegar execução para os construtores específicos via template_options

--- modular_template.rb
+++ modular_template.rb
@@ -22,5 +22,25 @@ def template_path(relative_path)
   end
 end
 
-say "modular_template.rb foi mantido por compatibilidade e delega para build_ddd.rb.", :yellow
-apply template_path("build_ddd.rb")
+apply template_path("shared/template_options.rb")
+apply template_path("shared/template_helpers.rb")
+
+selected_preset = ask_template_preset(default: "ddd")
+ask_include_user_setup
+
+say_info "Template selecionado: #{bold(selected_template_label)}"
+say_warn "Autenticação de usuários: #{@include_user_setup ? colorize('sim', :green) : colorize('não', :red)}"
+puts "[template] selected_preset=#{selected_preset}"
+puts "[template] include_user_auth=#{@include_user_setup}"
+
+build_script =
+  case selected_preset
+  when "api"
+    "build_api.rb"
+  when "mvc"
+    "build_mvc.rb"
+  else
+    "build_ddd.rb"
+  end
+
+apply template_path(build_script)
