913a2810fd227401986e135df4939c9150c194ef
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-06-01 17:04:20 -0300

    refactor: atualizar mensagem e fluxo de finalização do script de pós-instalação

--- shared/post_install.rb
+++ shared/post_install.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
+if @include_user_setup
+  if @selected_template_preset.to_s == "api"
+    apply_template "shared/api_identity_setup.rb"
+  else
+    apply_template "shared/auth_setup.rb"
+  end
+end
+
 say "Template aplicado com sucesso.", :green
 say "Revise os presets em build_*.rb para criar novos flavors rapidamente.", :blue
