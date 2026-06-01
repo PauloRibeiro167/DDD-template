85f1af3109ddc5a8bb20f92330a0bd21882f5567
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-06-01 17:04:54 -0300

    feat: adicionar setup genérico de autenticação compartilhada

---
+++ shared/auth_setup.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+say "Configurando Autenticação, Perfis e Permissões...", :green
+
+def application_record_path
+  "app/models/application_record.rb"
+end
+
+def ensure_application_record_exists
+  return if File.exist?(application_record_path)
+
+  empty_directory File.dirname(application_record_path)
+  create_file application_record_path, <<~RUBY
+    class ApplicationRecord < ActiveRecord::Base
+      primary_abstract_class
+    end
+  RUBY
+end
+
+ensure_application_record_exists
+
+generate "devise:install"
+generate "devise User"
+generate "model JwtDenylist jti:string:index exp:datetime --no-test-framework"
+
+inject_into_file "app/models/jwt_denylist.rb", after: "class JwtDenylist < ApplicationRecord\n" do
+  <<~RUBY
+    include Devise::JWT::RevocationStrategies::Denylist
+
+    self.table_name = 'jwt_denylists'
+  RUBY
+end
+
+inject_into_file "app/models/user.rb", after: "devise :database_authenticatable, :registerable,\n         :recoverable, :rememberable, :validatable" do
+  ",\n         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist"
+end
+
+inject_into_file "config/initializers/devise.rb", before: /^end$/ do
+  <<~RUBY
+
+    config.jwt do |jwt|
+      jwt.secret = ENV.fetch('DEVISE_JWT_SECRET_KEY') { Rails.application.credentials.secret_key_base }
+      jwt.dispatch_requests = [
+        ['POST', %r{^/login$}]
+      ]
+      jwt.revocation_requests = [
+        ['DELETE', %r{^/logout$}]
+      ]
+      jwt.expiration_time = 1.day.to_i
+    end
+  RUBY
+end
+
+generate "rolify Role User"
+generate "model Profile user:references first_name:string last_name:string phone:string document:string --no-test-framework"
+
+inject_into_file "app/models/user.rb", after: "class User < ApplicationRecord\n" do
+  "  has_one :profile, dependent: :destroy\n  accepts_nested_attributes_for :profile\n\n"
+end
+
+inject_into_file "app/models/user.rb", before: /^end$/ do
+  <<~RUBY
+
+    after_create :build_default_profile
+
+    private
+
+    def build_default_profile
+      create_profile unless profile
+    end
+  RUBY
+end
+
+say "Autenticação, Perfis e Permissões configurados com sucesso!", :blue
