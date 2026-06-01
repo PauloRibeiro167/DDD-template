744a7cd93805d00188d127e81af7ca95baa006ec
Paulo <paulo.rezende@sefin.fortaleza.ce.gov.br>
2026-06-01 17:04:34 -0300

    refactor: atualizar as configurações do zeitwerk para a API

--- presets/api_lite/zeitwerk.rb
+++ presets/api_lite/zeitwerk.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
+gsub_file "config/application.rb", 'config.autoload_lib(ignore: %w[assets tasks])', 'config.autoload_lib(ignore: %w[assets tasks generators])'
+
+api_domain_autoload_config = <<~RUBY
+      config.paths.add "app/domains", eager_load: true
+      config.paths.add "app/interfaces/http", eager_load: true
+      config.paths.add "app/infrastructure", eager_load: true
+
+RUBY
+
+unless File.read("config/application.rb").include?('config.paths.add "app/domains", eager_load: true')
+  inject_into_file "config/application.rb", after: "config.generators.system_tests = nil\n" do
+    api_domain_autoload_config
+  end
+end
+
 initializer "zeitwerk_api_lite.rb", <<~RUBY
-  domains_root = Rails.root.join("app/domains")
-  http_controllers_root = Rails.root.join("app/interfaces/http/controllers")
-  http_serializers_root = Rails.root.join("app/interfaces/http/serializers")
-
-  Rails.autoloaders.main.ignore(domains_root)
-  Rails.autoloaders.main.ignore(http_controllers_root)
-  Rails.autoloaders.main.ignore(http_serializers_root)
-
-  Rails.autoloaders.main.push_dir(domains_root)
-  Rails.autoloaders.main.push_dir(http_controllers_root)
-  Rails.autoloaders.main.push_dir(http_serializers_root)
+  Rails.autoloaders.main.push_dir(Rails.root.join("app/domains"))
+  Rails.autoloaders.main.push_dir(Rails.root.join("app/interfaces/http"))
+  Rails.autoloaders.main.push_dir(Rails.root.join("app/infrastructure"))
 RUBY
